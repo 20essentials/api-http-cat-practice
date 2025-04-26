ffff4e972a46854365ce8a8879143f26af15387e
DominicDev <s0l0estudi4r@gmail.com>
2025-04-26 21:35:14 +0200

    v0.0.6

--- index.js
+++ index.js
@@ -11,16 +11,27 @@ app.use(helmet());
 const __dirname = import.meta.dirname;
 const imageBasePath = join(__dirname, "public", "images");
 
-app.get("/:statusCode", (req, res, next) => {
-  const statusCode = Number(req.params.statusCode);
+app.get('/:statusCode(.+)', (req, res, next) => {
+  let statusCode = req.params.statusCode;
+
+  if (statusCode.endsWith('.jpg')) {
+    statusCode = statusCode.replace('.jpg', '');
+  }
+
+  statusCode = Number(statusCode);
   const existObject = LIST_ERRORS_HTTP.some(el => el.statusCode === statusCode);
   if (!existObject) return next();
+
   const image = join(imageBasePath, `${statusCode}.jpg`);
+  
+  res.type('jpg');
   res.status(200).sendFile(image);
 });
 
+
 app.use((_, res) => {
-  const image404 = join(imageBasePath, "404.jpg");
+  const image404 = join(imageBasePath, '404.jpg');
+  res.type('jpg');
   res.status(404).sendFile(image404);
 });
 
