3f3ae6bb11eb08cf7934a5d9903043b464f4eab9
DominicDev <s0l0estudi4r@gmail.com>
2025-04-26 21:24:31 +0200

    v0.0.4

--- index.js
+++ index.js
@@ -3,29 +3,31 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import { join } from 'node:path';
+import { fileURLToPath } from 'node:url'; // <- importante para __dirname en ESM
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = new URL('.', import.meta.url).pathname;
 
 const app = express();
 app.use(cors());
 app.use(helmet());
-const port = process.env.PORT ?? 8000;
-const __dirname = import.meta.dirname;
+
 const imageBasePath = join(__dirname, 'public', 'images');
 
+// Ruta que acepta /100 o /100.jpg
 app.get('/:statusCode(.+)', (req, res, next) => {
   const statusCode = Number(req.params.statusCode.replace('.jpg', ''));
   const existObject = LIST_ERRORS_HTTP.some(el => el.statusCode === statusCode);
   if (!existObject) return next();
-
   const image = join(imageBasePath, `${statusCode}.jpg`);
   res.status(200).sendFile(image);
 });
 
-
+// Manejo de 404
 app.use((_, res) => {
   const image404 = join(imageBasePath, '404.jpg');
   res.status(404).sendFile(image404);
 });
 
-app.listen(port, () => {
-  console.log(`Server Open in http://localhost:${port}`);
-});
+// 👇 Solo exportas el handler, no haces app.listen
+export default app;
